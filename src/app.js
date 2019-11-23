33e8a9c5ab4a9def1cbe56f091c3e35a05c3997f
JyChen97 <Jiayingchen1997@gmail.com>
2019-11-22 19:27:50 -0500

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -9,10 +9,7 @@ import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-
 const store = configureStore();
-
-console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
